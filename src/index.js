70c93e840dae6c84d940d1f3796d12b50d5d74d8
misspia <pia_leung@hotmail.com>
2019-07-08 13:58:18 -0400

    set up root react component, init themes folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('test')
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import styled, { createGlobalStyle } from 'styled-components';
+import { Colors } from './themes';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%:
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Container>
+          Pokedex
+        </Container>
+      </React.Fragment>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/themes/fonts.js
@@ -0,0 +1,5 @@
+export default {
+  family: '',
+  src: '',
+  weight: '',
+}
