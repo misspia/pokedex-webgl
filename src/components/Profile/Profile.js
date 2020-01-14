ca1e42adcf9905dd71e442d5d6a7b9e8e69d5fba
misspia <pia.leung@mediamonks.com>
2020-01-14 23:57:18 +0100

    Migrate to apollo hooks

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import * as THREE from 'three';
 import { gql } from 'apollo-boost';
 import { client } from '../../apollo';
@@ -7,11 +8,31 @@ import ProfileImage from '../ProfileImage/ProfileImage';
 import * as S from './Profile.styles';
 
 export const PROFILE_NAME = 'profile';
+const GET_POKEMON_BY_ID = gql`
+  query getPokemonById($id: PokemonId!) {
+    GetPokemonById(id: $id) {
+      id
+      name
+      chainId
+      types
+      height
+      weight
+      baseExperience
+      abilities
+      stats {
+        key
+        value
+      }
+      artworkUrl
+    }
+  }
+`;
 
 export default function Profile({
   active = false,
   id = null,
 }) {
+  const { loading, error, data} = useQuery(GET_POKEMON_BY_ID, { variables: { id: 1 }});
 
   // animate in/out
   useEffect(() => {
@@ -20,8 +41,8 @@ export default function Profile({
 
   // fetch new data
   useEffect(() => {
-    const pokemon = fetchPokemonById(id);
-    console.debug(pokemon);
+    // const pokemon = fetchPokemonById(id);
+    // console.debug(pokemon);
   }, [id]);
 
  return (
