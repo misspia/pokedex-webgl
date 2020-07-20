909860dd64f53d87264f7c6f089d2d551a335612
misspia <pia.leung@mediamonks.com>
2020-07-20 23:09:45 +0200

    disable user zoom/pan

--- src/webgl/Controls.js
+++ src/webgl/Controls.js
@@ -5,21 +5,17 @@ export default class Controls extends MapControls {
   constructor(context) {
     super(context.camera, context.renderer.domElement);
     this.context = context;
-    // this.controls = new MapControls(
-    //   this.context.camera,
-    //   this.context.renderer.domElement
-    // );
+    this.enableZoom = false;
+    this.enableRotate = false;
     // this.controls.enableDamping = true;
     // this.controls.dampingFactor = 0.05;
+  }
 
-    // this.controls.screenSpacePanning = false;
-    // this.controls.enableRotate = false;
-    // this.controls.enableZoom = false;
-    // this.controls.maxPolarAngle = Math.PI / 2;
+  enable() {
+    this.enabled = true;
+  }
 
-    // this.controls.minAzimuthAngle = -Math.PI;
-    // this.controls.maxAzimuthAngle = Math.PI;
-
-    // this.controls.update();
+  disabled() {
+    this.enabled = false;
   }
 }
